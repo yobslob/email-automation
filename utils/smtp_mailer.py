e0d625329e102f7c193fc21dbabeaec8c6967eb7
Yogesh <yogeshxiix@gmail.com>
2025-12-22 14:40:49 +0530

    Refactor SMTPMailer to include HTML to text conversion
    Added a static method to convert HTML to plain text and updated the send method to use it.

--- utils/smtp_mailer.py
+++ utils/smtp_mailer.py
@@ -1,8 +1,10 @@
-# utils/smtp_mailer.py
 import os
-import smtplib, ssl
+import re
+import smtplib
+import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.utils import make_msgid
 
 class SMTPMailer:
     def __init__(self):
@@ -12,13 +14,29 @@ class SMTPMailer:
         self.passwd = os.getenv('SMTP_PASS')
         self.from_name = os.getenv('FROM_NAME') or self.user
 
-    def send(self, to_email, subject, html_body, text_body=None):
+    @staticmethod
+    def _html_to_text(html_body: str) -> str:
+        text = (html_body or '')
+        text = re.sub(r'<br\s*/?>', '\n', text, flags=re.IGNORECASE)
+        text = re.sub(r'<[^>]+>', '', text)
+        return text
+
+    def send(self, to_email, subject, html_body, text_body=None,
+             in_reply_to=None, message_id=None):
         if text_body is None:
-            text_body = (html_body or '').replace('<br>', '\n').replace('<[^>]+>', '')
+            text_body = self._html_to_text(html_body)
+
         msg = MIMEMultipart('alternative')
         msg['From'] = f"{self.from_name} <{self.user}>"
         msg['To'] = to_email
         msg['Subject'] = subject
+
+        mid = message_id or make_msgid(domain=self.user.split('@')[-1])
+        msg['Message-ID'] = mid
+        if in_reply_to:
+            msg['In-Reply-To'] = in_reply_to
+            msg['References'] = in_reply_to
+
         msg.attach(MIMEText(text_body, 'plain'))
         msg.attach(MIMEText(html_body, 'html'))
 
@@ -27,3 +45,5 @@ class SMTPMailer:
             server.starttls(context=context)
             server.login(self.user, self.passwd)
             server.sendmail(self.user, to_email, msg.as_string())
+
+        return mid
